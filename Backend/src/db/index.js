b15eca5a3f8fe5e92d81d2f469caca10b0ef2a8e
Puneethkrishna-2003 <rspuneeth30@gmail.com>
2025-08-03 15:31:29 +0530

    Backend DB connected

---
+++ Backend/src/db/index.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import express from "express";
+import { dbName } from "../constants.js";
+
+const app = express();
+
+export const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(
+      `${process.env.MONGO_DB_URI}/${dbName}`
+    );
+    console.log("mongoDB connected successfully");
+
+    app.on("error", (error) => {
+      console.log(error);
+    });
+
+    app.listen(3000, () => {
+      console.log("App is listing on port 3000");
+      console.log(connection.connection.host);
+    });
+  } catch (error) {
+    console.error("mongoDB connection error " + error);
+  }
+};
