a6a7f842fa019a056efe266e4c6c94a8a65fdeaa
PuneethKrishnaS <rspuneeth30@gmail..com>
2025-09-10 19:55:53 +0530

    production setup fix

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,22 +1,7 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ['@vladmandic/face-api', '@tensorflow/tfjs'] 
-    }
-  },
-  optimizeDeps: {
-    include: ['@tensorflow/tfjs', '@vladmandic/face-api']
-  }
-})
+  plugins: [react()],
+});
